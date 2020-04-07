0598d93d66b5bd4dc1ebc9c802c9e2a591181e20
mingxiaoyu <fengying0347@163.com>
2020-04-07 14:47:44 +0800

    Add files via upload

---
+++ diy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+
+echo '修改机器名称'
+sed -i 's/OpenWrt/MiWiFi-R3P/g' package/base-files/files/bin/config_generate
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+echo '更改默认主题为Argon'
+git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/mine/luci-theme-argon_new
+
+echo '去除默认bootstrap主题'
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+#sed -i "s/114.114.114.114,114.114.115.115/114.114.114.114,223.5.5.5/g" package/lean/luci-app-flowoffload/root/etc/init.d/flowoffload
+#sed -i "s/114.114.114.114,114.114.115.115/114.114.114.114,223.5.5.5/g" package/lean/luci-app-flowoffload/luasrc/model/cbi/flowoffload.lua
+
+echo '网易云音乐'
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/mine/luci-app-unblockneteasemusic
+
+# disable usb3.0
+# git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/mine/luci-app-usb3disable
+
+mkdir -p package/luci-app-diskman && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O package/luci-app-diskman/Makefile
+mkdir -p package/parted && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
