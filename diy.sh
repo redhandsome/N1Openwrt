28dd00b42c7ca4df30058ee5e9ac1a90656cc353
Bosheng.Chen <Bosheng.Chen@infosys.com>
2020-04-10 10:36:24 +0800

    N1 compose files

--- diy.sh
+++ diy.sh
@@ -9,14 +9,18 @@
 echo '修改机器名称'
 sed -i 's/OpenWrt/MiWiFi-R3P/g' package/base-files/files/bin/config_generate
 
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
+
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+# firewall custom
+echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
 
 echo '更改默认主题为Argon'
 git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/mine/luci-theme-argon_new
 
-echo '去除默认bootstrap主题'
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 echo '网易云音乐'
 git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/mine/luci-app-unblockneteasemusic
